faad3276f034a4a726d2b1f15d7d91e62af5b876
ericpkatz <ericpkatz@gmail.com>
2021-04-01 20:48:19 -0400

    added server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const faker = require('faker');
 const { DataTypes: { STRING }} = Sequelize;
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/the-facet-store');
 
@@ -24,6 +25,10 @@ const seed = async()=> {
 
 
 const port = process.env.PORT || 3000;
+
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
 
 app.get('/api/products/:filter?', async(req, res, next)=> {
   try {
